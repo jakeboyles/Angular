0c38678a3ed310b12402b5b3dd65ef432c3bb421
Jake Boyles <jake.boyles@docustar.com>
2014-04-04 15:26:23 -0400

    fixes

--- project.js
+++ project.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('project', ['ratings','ngRoute','firebase','angularFileUpload','wu.masonry'])
 
 .value('fbURL', 'https://amber-fire-7013.firebaseio.com/projects/')
@@ -26,7 +28,7 @@ $routeProvider
 })
 
 .factory('Projects', function($firebase, fbURL, $timeout, $q) {
-        return $firebase(new Firebase(fbURL));
+    return $firebase(new Firebase(fbURL));
 })
 
 .factory('Facebook', function($http) {
@@ -43,116 +45,115 @@ $routeProvider
         logIn:function () {
             var status ="";
             FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-                status = uid;
-          } else if (response.status === 'not_authorized') {
-                status = false;
-          } else {
-                status = false;
-          }
-        });
-        return status;
+                if (response.status === 'connected') {
+                    var uid = response.authResponse.userID;
+                    var accessToken = response.authResponse.accessToken;
+                    status = uid;
+              } else if (response.status === 'not_authorized') {
+                    status = false;
+              } else {
+                    status = false;
+              }
+            });
+            return status;
         }
     }
-    })
+})
 
 
 .factory ('Model', function ($firebase,fbURL, Projects) {
-            return {
-                projects:function () {
-                    return Projects;
-                },
-                get:function(theID){
-                  return Projects[theID];
-                },
-                add:function (headline,content,type) {
-                    var currentIndex = data.length;
-                    var project = new Object();
-                },
-                edit:function (id,headline,content,type) {
-                    data[id].type =type;
-                    data[id].content =content;
-                    data[id].headline = headline; 
-                },
-                delete:function (id) {
-                    var oldNotes = data;
-                    data = [];
-                    angular.forEach(oldNotes, function (note) {
-                        if (note.id !== id) data.push(note);
-                    });
-                }
-            }
-        })
-
-
-function mainCtrl($scope,$location,$routeParams, Model, Projects,fbURL, $firebase, $timeout,Facebook) {
-
-        $timeout(function() {
-            if(Facebook.logIn()!="0"){
-                $scope.loginButton = "Logout";
-            }
-            else {
-                $scope.loginButton = "Login"
-            }
-        },500)
-
-         $scope.fbLogin = function() {
-            if(Facebook.logIn()!=false){
-                FB.logout();
-            }
-            else {
-                FB.login();
-                $scope.loginButton = "Logout";
-            }
-         }
-
-        $scope.projects = Model.projects();
-        $scope.deleteProject = function(id) {
-            var projectUrl = fbURL + id;
-            $scope.project = $firebase(new Firebase(projectUrl));
-            $scope.project.$remove();
+    return {
+        projects:function () {
+            return Projects;
+        },
+        get:function(theID){
+            return Projects[theID];
+        },
+        add:function (headline,content,type) {
+            var currentIndex = data.length;
+            var project = new Object();
+        },
+        edit:function (id,headline,content,type) {
+            data[id].type =type;
+            data[id].content =content;
+            data[id].headline = headline; 
+        },
+        delete:function (id) {
+            var oldNotes = data;
+            data = [];
+            angular.forEach(oldNotes, function (note) {
+                if (note.id !== id) data.push(note);
+            });
         }
-
-
     }
+})
+
+
+function mainCtrl($scope,$location,$routeParams, Model, Projects,fbURL, $firebase, $timeout,Facebook,$rootScope) {
+
+    $timeout(function() {
+        if(Facebook.logIn()!="0"){
+            $scope.loginButton = "Logout";
+        }
+        else {
+            $scope.loginButton = "Login"
+        }
+    },500)
+
+     $scope.fbLogin = function() {
+        if(Facebook.logIn()!=false){
+            FB.logout();
+        }
+        else {
+            FB.login();
+            $scope.loginButton = "Logout";
+        }
+     }
+
+    $scope.projects = Model.projects();
+    $scope.deleteProject = function(id) {
+        var projectUrl = fbURL + id;
+        $scope.project = $firebase(new Firebase(projectUrl));
+        $scope.project.$remove();
+    }
+
+}
 
 function projectCtrl($scope,$routeParams, Model,Projects,fbURL, $firebase, $timeout) {
-         $timeout(function() { 
-            $scope.project = Projects[$routeParams.projectid];
-            $scope.id = $routeParams.projectid;
-          },200);
-    }
+    $timeout(function() { 
+        $scope.project = Projects[$routeParams.projectid];
+        $scope.id = $routeParams.projectid;
+    },200);
+}
 
 
 function addCtrl($scope, $upload, $location, Model, Projects, $timeout,$firebase,Facebook) {
 
-        $scope.addTodo = function() {
+    $scope.addTodo = function() {
         Facebook.askFacebookForBio(
-        function(reason) { // fail
-            $scope.error = reason;
-        }, function(user) { // success
-            $scope.project.userName = user.name;
-            $scope.$digest() // Manual scope evaluation
-            Projects.$add($scope.project, function() {
-            $timeout(function() { $location.path('/'); });
-        });
-        })
+            function(reason) { // fail
+                $scope.error = reason;
+                }, function(user) { // success
+                    $scope.project.userName = user.name;
+                    $scope.$digest() // Manual scope evaluation
+                    Projects.$add($scope.project, function() {
+                        $timeout(function() { $location.path('/'); });
+                    });
+                }
+    )}
+
+    $scope.onFileSelect = function($files) {
+        $(".imagePreview").show();
+
+        var file = $files[0];
+
+        if (file.type.indexOf('image') == -1) {
+            $scope.error = 'image extension not allowed, please choose a JPEG or PNG file.' ;
         }
 
-
-        $scope.onFileSelect = function($files) {
-        $(".imagePreview").show();
-        var file = $files[0];
-            if (file.type.indexOf('image') == -1) {
-                 $scope.error = 'image extension not allowed, please choose a JPEG or PNG file.' ;
-                 alert($scope.error);
-            }
-            if (file.size > 2097152){
-                 $scope.error ='File size cannot exceed 2 MB';
-                 alert($scope.error);
-            }
+        if (file.size > 2097152){
+            $scope.error ='File size cannot exceed 2 MB';
+        }
 
         if(!$scope.error) {
             $scope.upload = $upload.upload({
@@ -167,30 +168,23 @@ function addCtrl($scope, $upload, $location, Model, Projects, $timeout,$firebase
                 $scope.project.file = data;
             });
         }
+
         else {
             $scope.error=null;
         }
-        }
-
-    };
+    }
+};
 
 function editCtrl($scope, $location, Model, $routeParams, Projects,fbURL, $timeout, $firebase) {
     $timeout(function() { 
-            var projectUrl = fbURL + $routeParams.projectid;
-            $scope.project = $firebase(new Firebase(projectUrl));
-            $scope.id = $routeParams.projectId;
+        var projectUrl = fbURL + $routeParams.projectid;
+        $scope.project = $firebase(new Firebase(projectUrl));
+        $scope.id = $routeParams.projectId;
 
-          },200);
+    },200);
 
-         $scope.editProjects = function() {
-                 $scope.project.$save();
-                 $location.path('/')
-                }  
-        }
-
-
-
-
-
-
-
+    $scope.editProjects = function() {
+        $scope.project.$save();
+        $location.path('/')
+    }  
+}
