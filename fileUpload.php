5aaad90594588dfcb56d77cdfe17aecffb955998
Jake Boyles <jake.boyles@docustar.com>
2014-04-09 11:52:48 -0400

    s3

--- fileUpload.php
+++ fileUpload.php
@@ -12,7 +12,7 @@ if (in_array($extension, $allowedExts))
   else
     {
 
-    if (file_exists("uploads/" . $_FILES["file"]["name"]))
+    if (2==1)
       {
       echo $_FILES["file"]["name"];
       }
@@ -25,9 +25,47 @@ if (in_array($extension, $allowedExts))
 
       $temper = rand();
 
+      $newFile = "uploads/".$temper . urlencode($_FILES["file"]["name"]);
+
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "uploads/".$temper . $_FILES["file"]["name"]);
-      echo $temper.$_FILES["file"]["name"];
+      $newFile);
+
+      if (!class_exists('S3')) require_once 'S3.php';
+
+      // AWS access info
+      if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAIYHS42A3YNAGOG3Q');
+      if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+
+      $uploadFile = dirname(__FILE__)."/".$newFile; // File to upload, we'll use the S3 class since it exists
+      $bucketName = 'ChowPics'; // Temporary bucket
+
+      // If you want to use PECL Fileinfo for MIME types:
+      //if (!extension_loaded('fileinfo') && @dl('fileinfo.so')) $_ENV['MAGIC'] = '/usr/share/file/magic';
+
+
+      // Check if our upload file exists
+      if (!file_exists($uploadFile) || !is_file($uploadFile))
+      exit("\nERROR: No such file: $uploadFile\n\n");
+
+      // Check for CURL
+      if (!extension_loaded('curl') && !@dl(PHP_SHLIB_SUFFIX == 'so' ? 'curl.so' : 'php_curl.dll'))
+      exit("\nERROR: CURL extension not loaded\n\n");
+
+      // Pointless without your keys!
+      if (awsAccessKey == 'change-this' || awsSecretKey == 'change-this')
+      exit("\nERROR: AWS access information required\n\nPlease edit the following lines in this file:\n\n".
+      "define('awsAccessKey', 'change-me');\ndefine('awsSecretKey', 'change-me');\n\n");
+
+      // Instantiate the class
+      $s3 = new S3(awsAccessKey, awsSecretKey);
+
+      // Put our file (also with public read access)
+      if ($s3->putObjectFile($uploadFile, $bucketName, baseName($uploadFile), S3::ACL_PUBLIC_READ)) {
+      echo "https://s3.amazonaws.com/ChowPics/".baseName($uploadFile).PHP_EOL;
+      } else {
+      echo "S3::putBucket(): Unable to create bucket (it may already exist and/or be owned by someone else)\n";
+      }
+
       }
     }
   }
